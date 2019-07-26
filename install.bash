6f89f78c48203a450a1b2c23528f6f9e73029e74
Morten Daniel Fornes <mdfornes@gmail.com>
2019-07-27 01:07:47 +0700

    check that detected Mono is compatible (>= v5.4.1.7)

--- install.bash
+++ install.bash
@@ -1,33 +1,53 @@
-MONO_VERSION="6.0.0.311"
+if [ -z "$DOTNET_RUN_HOME" ]; then
+    DOTNET_RUN_HOME=~/.dotnet-run
+fi
+
+if [ -z "$MIN_MONO_VERSION" ]; then
+    MIN_MONO_VERSION="5.4.1.7"
+fi
 
 # Begin script
 cd "`dirname "$0"`" || exit 1
 set -e
 
 # Detect installed Mono
-function test-mono {
-    if which $1 > /dev/null 2>&1; then
-        ln -sfv `which $1` dotnet-run
-        exit 0
+function version-gte {
+    if [ -z "$2" ]; then
+        return 0
+    fi
+
+    local version=`"$1" --version | head -n1 | egrep -o '[0-9].*-[0-9]' | awk '{print $1;}'`
+    node version-gte.js "$version" "$2"
+}
+
+function success-if-compatible {
+    if which "$1" > /dev/null 2>&1; then
+        if version-gte "$1" "$MIN_MONO_VERSION"; then
+            ln -sfv "`which "$1"`" dotnet-run
+            exit 0
+        fi
     fi
 }
 
-if [ "$DETECT_MONO" != 0 ]; then
-    if [ -f dotnet-run ]; then
+if [ "$FORCE_MONO_DOWNLOAD" != 1 ]; then
+    if [ -f ./dotnet-run ]; then
         exit 0
     fi
-    test-mono mono64
-    test-mono mono
+
+    success-if-compatible "$DOTNET_RUN_HOME/mono/bin/mono"
+    success-if-compatible mono64
+    success-if-compatible mono
 fi
 
 # Detect platform
 case "$(uname -s)" in
 Darwin)
+    MONO_VERSION="6.0.0.311"
     MONO_URL=https://github.com/mortend/dotnet-run/releases/download/mono-$MONO_VERSION-macOS/mono-$MONO_VERSION-macOS.tgz
     ;;
 *)
-    test-mono mono
-    echo -e "ERROR: Mono was not found" >&2
+    success-if-compatible mono
+    echo -e "ERROR: Mono (>= $MIN_MONO_VERSION) was not found." >&2
     echo -e "\nPlease follow instructions at https://www.mono-project.com/download/ to install Mono, and try again." >&2
     exit 1
     ;;
@@ -48,7 +68,9 @@ function get-tgz {
     if [ -f "$tgz" ]; then
         rm -rf "$dir" "$tgz"
     elif [ -d "$dir" ]; then
-        return
+        if version-gte "$dir/mono/bin/mono" $MONO_VERSION; then
+            return
+        fi
     fi
 
     echo "Downloading $url"
@@ -58,9 +80,6 @@ function get-tgz {
     rm -rf "$tgz"
 }
 
-if [ -z "$DOTNET_RUN_HOME" ]; then
-    DOTNET_RUN_HOME=~/.dotnet-run
-fi
-
 get-tgz "$MONO_URL" "$DOTNET_RUN_HOME"
-ln -sfv "$DOTNET_RUN_HOME/mono/bin/mono" dotnet-run
+success-if-compatible "$DOTNET_RUN_HOME/mono/bin/mono"
+download-error
