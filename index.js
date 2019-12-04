5ae3f298352581b11fdaf4bb7eef3b1e0e418bdd
Morten Daniel Fornes <mdfornes@gmail.com>
2019-12-04 14:22:12 +0100

    expand ~ to process.env.HOME

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const path = require('path');
 const {spawn} = require('child_process');
 
 function dotnet_spawn(filename, args) {
-    const home = process.env.DOTNET_RUN_HOME || '~/.dotnet-run'
-    const mono = path.join(home, '.bin', 'mono');
     const options = {stdio: 'inherit'};
 
     if (path.sep == '\\')
@@ -11,6 +9,10 @@ function dotnet_spawn(filename, args) {
     else {
         if (filename)
             args.unshift(filename);
+
+        const home = process.env.DOTNET_RUN_HOME ||
+                     path.join(process.env.HOME, '.dotnet-run');
+        const mono = path.join(home, '.bin', 'mono');
         return spawn(mono, args, options);
     }
 }
