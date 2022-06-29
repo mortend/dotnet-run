a436e544387d62b247b001ea0e3ee0d140a0e663
Morten Daniel Fornes <morten@fusetools.com>
2022-06-29 21:25:57 +0700

    add fallback to install script

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 const path = require('path');
 const {spawn} = require('child_process');
 
-function dotnet_spawn(filename, args) {
+module.exports = (filename, args, callback) => {
     const options = {stdio: 'inherit'};
 
     if (path.sep == '\\')
-        return spawn(filename, args, options);
+        spawn(filename, args, options).on('exit', callback);
     else {
         if (filename)
             args.unshift(filename);
@@ -13,9 +14,21 @@ function dotnet_spawn(filename, args) {
         const home = process.env.DOTNET_RUN_HOME ||
                      path.join(process.env.HOME, '.dotnet-run');
         const mono = path.join(home, '.bin', 'mono');
-        return spawn(mono, args, options);
+
+        if (fs.existsSync(mono))
+            spawn(mono, args, options).on('exit', callback);
+        else {
+            // Fallback to install script.
+            spawn('bash', [
+                path.join(__dirname, 'src', 'install.bash')
+            ], {
+                stdio: 'inherit'
+            }).on('exit', code => {
+                if (code == 0)
+                    spawn(mono, args, options).on('exit', callback);
+                else if (callback)
+                    callback(code)
+            });
+        }
     }
 }
-
-module.exports = (filename, args, callback) =>
-    dotnet_spawn(filename, args).on('exit', callback);
