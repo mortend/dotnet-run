ba0be229b6755731fca7f09800002619882fd032
Morten Daniel Fornes <morten@fusetools.com>
2022-09-18 17:49:55 +0700

    Move to 'dotnet' tool (for .NET 6.0)
    Now using the modern 'dotnet' tool to run programs rather than
    depending on the aging Mono Project.
    
    NB! This is a breaking change since we're no longer able to run
    .NET Framework programs. Please use dotnet-run@1.x as needed.
    
    * Update README
    * Update test
    * Drop install.js
    * Drop mono-gen
    * Drop travis

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const {spawn} = require('child_process');
+const { spawn } = require("child_process")
 
 module.exports = (filename, args, callback) => {
-    const options = {stdio: 'inherit'};
+    if (filename)
+        args.unshift(filename)
 
-    if (path.sep == '\\')
-        spawn(filename, args, options).on('exit', callback);
-    else {
-        if (filename)
-            args.unshift(filename);
-
-        const home = process.env.DOTNET_RUN_HOME ||
-                     path.join(process.env.HOME, '.dotnet-run');
-        const mono = path.join(home, '.bin', 'mono');
-
-        if (fs.existsSync(mono))
-            spawn(mono, args, options).on('exit', callback);
-        else {
-            // Fallback to install script.
-            spawn('bash', [
-                path.join(__dirname, 'src', 'install.bash')
-            ], {
-                stdio: 'inherit'
-            }).on('exit', code => {
-                if (code == 0)
-                    spawn(mono, args, options).on('exit', callback);
-                else if (callback)
-                    callback(code)
-            });
-        }
-    }
+    spawn("dotnet", args, { stdio: "inherit" })
+        .on("exit", callback)
 }
