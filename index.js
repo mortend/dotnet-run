f9078cf6dc05f337f7ebabef49b6a0576df42267
Morten Daniel Fornes <morten@fusetools.com>
2022-09-24 13:58:15 +0700

    Suggest solutions
    Suggest possible solutions in cases where the dotnet command isn't
    detected.

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
+const which = require("which")
 const { spawn } = require("child_process")
 
-module.exports = (filename, args, callback) => {
-    if (filename)
-        args.unshift(filename)
+function getDotNetPath() {
+    for (const dotnet of which.sync("dotnet", {
+        all: true,
+        nothrow: true
+    }) || []) {
+        return dotnet
+    }
+}
 
-    spawn("dotnet", args, { stdio: "inherit" })
-        .on("exit", callback)
+function suggestSolutions() {
+    console.log("\nPossible solutions")
+    console.log("------------------\n")
+
+    if (process.platform === "darwin") {
+        console.log("* Install .NET Runtime using Homebrew:\n")
+        console.log("    brew install dotnet\n")
+    } else if (process.platform === "win32") {
+        console.log("* Install .NET Runtime using Chocolatey:\n")
+        console.log("    choco install dotnet\n")
+    } else {
+        console.log("* Install .NET Runtime using your package manager (Ubuntu):\n")
+        console.log("    sudo apt-get install -y dotnet-runtime-6.0\n")
+    }
+
+    console.log("* Get .NET Runtime from Microsoft:\n")
+    console.log("    https://dotnet.microsoft.com/download/dotnet-core/6.0\n")
+}
+
+module.exports = (filename, args, callback) => {
+    const dotnet = getDotNetPath()
+
+    if (dotnet) {
+        if (filename)
+            args.unshift(filename)
+
+        spawn(dotnet, args, { stdio: "inherit" })
+            .on("exit", callback)
+    } else {
+        console.error("fatal: The 'dotnet' command was not found")
+        suggestSolutions()
+        callback(0x7f)
+    }
 }
