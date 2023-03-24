662509e269684a641e3202209a914328a04bb66d
Morten Daniel Fornes <morten@fusetools.com>
2023-03-25 00:36:28 +0700

    Make index.d.ts compile with tsc

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,13 @@
-declare module "dotnet-run" {
+/** Run a dotnet assembly */
+declare function runDotNet(filename: string | undefined, args: string[], callback: (result?: number) => void): void
 
-    /** Run a dotnet assembly */
-    function run(filename: string | undefined, args: string[], callback: (result?: number) => void): void
-
+/** Sub functions */
+declare namespace runDotNet {
     /** Get a full path to the 'dotnet' command, or undefined if not found */
     function getDotNetPath(): string | undefined
 
     /** Run the dotnet install script */
     function installDotNet(callback: (result?: number) => void): void
-
-    run.getDotNetPath = getDotNetPath
-    run.installDotNet = installDotNet
-    export = run
 }
+
+export = runDotNet
