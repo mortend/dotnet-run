ff53e2fd1d789a586ca2cf11fbee9b521684459d
Morten Daniel Fornes <morten@fusetools.com>
2023-03-25 18:35:30 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 /** Run a dotnet assembly */
-declare async function runDotNet(filename: string | undefined, args?: string[]): Promise<number?>
+declare function run(filename: string | undefined, args?: string[]): Promise<number>
 
 /** Sub functions */
-declare namespace runDotNet {
+declare namespace run {
     /** Get a full path to the 'dotnet' command, or undefined if not found */
     function getDotNetPath(): string | undefined
 
     /** Run the dotnet install script */
-    async function installDotNet(): Promise<number?>
+    function installDotNet(): Promise<number>
 }
 
-export = runDotNet
+export = run
